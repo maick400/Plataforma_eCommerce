953c1f53835d1d2f8d12f58793974064e646ef83
maick400 <maick400@hotmail.com>
2025-06-11 22:29:37 +0000

    Credencas as

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://devpinaec.us-east-2.elasticbeanstalk.com/tienda';
+    public string $baseURL = 'http://devpinaec.us-east-2.elasticbeanstalk.com';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
